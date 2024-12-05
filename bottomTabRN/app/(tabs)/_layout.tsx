0f240fad8728066fedcec8b574c3040b71ba0792
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2024-12-05 13:45:42 +0530

    did initial setup and added 3 tabs at bottom

---
+++ bottomTabRN/app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import {Tabs} from 'expo-router'
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const TabLayout = () => {
+    return(
+        <Tabs/>
+    )
+}
+
+
+export default TabLayout
